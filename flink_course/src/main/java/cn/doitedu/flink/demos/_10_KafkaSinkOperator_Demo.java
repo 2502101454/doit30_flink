5527488f580300b40f41ead1b9fcdd0b21a33889
wangzeng <zeng.wangz@zenjoy.net>
2023-04-25 09:06:14 +0800

    add kafkasink

---
+++ flink_course/src/main/java/cn/doitedu/flink/demos/_10_KafkaSinkOperator_Demo.java
@@ -0,0 +1,46 @@
+package cn.doitedu.flink.demos;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.connector.base.DeliveryGuarantee;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @author zengwang
+ * @create 2023-04-24 19:51
+ * @desc:
+ */
+public class _10_KafkaSinkOperator_Demo {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration();
+        configuration.setInteger("rest.port", 8822);
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(configuration);
+
+        // 开启Checkpoint
+        env.enableCheckpointing(5000, CheckpointingMode.EXACTLY_ONCE);
+        env.getCheckpointConfig().setCheckpointStorage("");
+
+        DataStreamSource<EventLog> streamSource = env.addSource(new MySourceFunction());
+
+        // 构造kafka sink算子
+        KafkaSink<String> kafkaSink = KafkaSink.<String>builder()
+                .setBootstrapServers("hadoop102:9092,hadoop103:9092,hadoop104:9092")
+                .setRecordSerializer(KafkaRecordSerializationSchema.<String>builder()
+                        .setTopic("event-log")
+                        .setValueSerializationSchema(new SimpleStringSchema())
+                        .build()
+                )
+                .setDeliverGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
+                .setTransactionalIdPrefix("doitedu-")
+                .build();
+
+        streamSource.map(JSON::toJSONString)
+                .sinkTo(kafkaSink);
+
+    }
+}
