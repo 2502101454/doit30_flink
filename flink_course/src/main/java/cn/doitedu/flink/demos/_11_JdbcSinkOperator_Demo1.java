72f9bd083cffa6abee18786170aee1a6cc3b04f3
wangzeng <zeng.wangz@zenjoy.net>
2023-04-26 19:04:19 +0800

    update jdbc and redis sink

--- flink_course/src/main/java/cn/doitedu/flink/demos/_11_JdbcSinkOperator_Demo1.java
+++ flink_course/src/main/java/cn/doitedu/flink/demos/_11_JdbcSinkOperator_Demo1.java
@@ -1,16 +1,16 @@
 package cn.doitedu.flink.demos;
 
 import com.alibaba.fastjson.JSON;
+import com.mysql.cj.jdbc.MysqlXADataSource;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
-import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
-import org.apache.flink.connector.jdbc.JdbcSink;
-import org.apache.flink.connector.jdbc.JdbcStatementBuilder;
+import org.apache.flink.connector.jdbc.*;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.util.function.SerializableSupplier;
 
+import javax.sql.XADataSource;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -39,7 +39,7 @@ public class _11_JdbcSinkOperator_Demo1 {
 
         DataStreamSource<EventLog> streamSource = env.addSource(new MySourceFunction());
 
-        // 构造一个JdbcSink算子
+        // 构造一个JdbcSink算子，不保证EOC，因为底层mysql的`producer` 即connection 没有开启事务
         SinkFunction<EventLog> jdbcSink = JdbcSink.sink("insert into t_eventlog values(?, ?, ?, ?, ?) on duplicate" +
                         " key update sessionId=?,eventId=?,ts=?,eventInfo=?",
                 new JdbcStatementBuilder<EventLog>() {
@@ -64,7 +64,42 @@ public class _11_JdbcSinkOperator_Demo1 {
                         .build()
         );
 
-        streamSource.addSink(jdbcSink);
+//        streamSource.addSink(jdbcSink);
+
+        // 构造一个JdbcSink算子对象,保证EOC的一种，底层利用jdbc目标数据库的事务机制
+        SinkFunction<EventLog> JdbcEocSink = JdbcSink.exactlyOnceSink("insert into t_eventlog values(?, ?, ?, ?, ?) on duplicate" +
+                        " key update sessionId=?,eventId=?,ts=?,eventInfo=?",
+                new JdbcStatementBuilder<EventLog>() {
+                    @Override
+                    public void accept(PreparedStatement stmt, EventLog eventLog) throws SQLException {
+                        stmt.setLong(1, eventLog.getGuid());
+                        stmt.setString(2, eventLog.getSessionId());
+                        stmt.setString(3, eventLog.getEventId());
+                        stmt.setLong(4, eventLog.getTimestamp());
+                        stmt.setString(5, JSON.toJSONString(eventLog.getEventInfo()));
+                        stmt.setString(6, eventLog.getSessionId());
+                        stmt.setString(7, eventLog.getEventId());
+                        stmt.setLong(8, eventLog.getTimestamp());
+                        stmt.setString(9, JSON.toJSONString(eventLog.getEventInfo()));
+                    }
+                },
+                JdbcExecutionOptions.builder().withBatchSize(5).withMaxRetries(2).build(),
+                // mysql不支持一个连接上并行多个事务，必须设置此参数为true
+                JdbcExactlyOnceOptions.builder().withTransactionPerConnection(true).build(),
+                // XaDataSource就是Jdbc连接，不过它是支持分布式事务的连接，它的构造方法，不同的数据是不同的
+                new SerializableSupplier<XADataSource>() {
+                    @Override
+                    public XADataSource get() {
+                        MysqlXADataSource xaDataSource = new MysqlXADataSource();
+                        xaDataSource.setUrl("jdbc:mysql://localhost:3306/wz_test");
+                        xaDataSource.setUser("root");
+                        xaDataSource.setPassword("12345678");
+                        return xaDataSource;
+                    }
+                }
+        );
+
+        streamSource.addSink(JdbcEocSink);
         env.execute();
     }
 }
---
+++ flink_course/src/main/java/cn/doitedu/flink/demos/_12_RedisSinkOperator_Demo.java
@@ -0,0 +1,119 @@
+package cn.doitedu.flink.demos;
+
+import com.alibaba.fastjson.JSON;
+import com.mysql.cj.jdbc.MysqlXADataSource;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.connector.jdbc.*;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.streaming.connectors.redis.RedisSink;
+import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisClusterConfig;
+import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
+import org.apache.flink.util.function.SerializableSupplier;
+
+import javax.sql.XADataSource;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Optional;
+
+/**
+ * @author zengwang
+ * @create 2023-04-25 23:22
+ * @desc:
+ * CREATE TABLE t_eventlog (
+ * guid bigint NOT NULL AUTO_INCREMENT,
+ * sessionId varchar(100) DEFAULT NULL,
+ * eventId varchar(100) DEFAULT NULL,
+ * ts bigint DEFAULT NULL,
+ * eventInfo varchar(100) DEFAULT NULL,
+ * PRIMARY KEY (guid)
+ * ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+ */
+public class _12_RedisSinkOperator_Demo {
+    public static void main(String[] args) throws Exception {
+        Configuration configuration = new Configuration();
+        configuration.setInteger("rest.port", 8822);
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(configuration);
+
+        env.setParallelism(2);
+        // 开启Checkpoint
+        env.enableCheckpointing(5000, CheckpointingMode.EXACTLY_ONCE);
+        env.getCheckpointConfig().setCheckpointStorage("file:///Users/zeng.wang/Downloads/ckpt");
+
+        DataStreamSource<EventLog> streamSource = env.addSource(new MySourceFunction());
+
+//        streamSource.print();
+        // eventLog数据插入redis
+        FlinkJedisPoolConfig config = new FlinkJedisPoolConfig.Builder().setHost("localhost").build();
+        // 构建redisSink算子
+        RedisSink<EventLog> redisSink = new RedisSink<>(config, new StringInsertMapper());
+        streamSource.addSink(redisSink);
+
+        env.execute();
+    }
+
+    /**
+     * String 类型写入
+     */
+    static class StringInsertMapper implements RedisMapper<EventLog> {
+        @Override
+        public RedisCommandDescription getCommandDescription() {
+            // 数据存String类型
+            return new RedisCommandDescription(RedisCommand.SET);
+        }
+
+        /**
+         * 指定要插入的大key，redis的大 key数量是没有上限，list、map这类其中要存的元素个数也没有上限，只要内存足够
+         * @param eventLog
+         * @return
+         */
+        @Override
+        public String getKeyFromData(EventLog eventLog) {
+            return eventLog.getSessionId();
+        }
+
+        /**
+         * 指定大key对应的值
+         * @param eventLog
+         * @return
+         */
+        @Override
+        public String getValueFromData(EventLog eventLog) {
+            return JSON.toJSONString(eventLog);
+        }
+    }
+
+    /**
+     * Hash类型写入
+     */
+    static class HashInsertMapper implements RedisMapper<EventLog> {
+
+        @Override
+        public RedisCommandDescription getCommandDescription() {
+            return new RedisCommandDescription(RedisCommand.HSET, "event-logs");
+        }
+
+        /**
+         * 1.对于有内部 小key的数据结构，比如hash，则大key由上面RedisCommandDescription中的additionalKey指定
+         *      小key又此处指定
+         *
+         * 2.对于内部 没有小key的，比如list、set，则大key由此处指定，上面指定也无效
+         * @param eventLog
+         * @return
+         */
+        @Override
+        public String getKeyFromData(EventLog eventLog) {
+            return eventLog.getSessionId();
+        }
+
+        @Override
+        public String getValueFromData(EventLog eventLog) {
+            return JSON.toJSONString(eventLog); // 小key的value
+        }
+    }
+}
